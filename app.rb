a381fe0044b405a1d1e93fbe89c9f2b6339595a1
kazuki0204 <ka_ki0204@icloud.com>
2021-08-30 06:19:00 +0000

    Implement feature: creating new counter

--- app.rb
+++ app.rb
@@ -8,3 +8,29 @@ before do
     Count.create(number: 0)
   end
 end
+
+get '/' do
+  @number = Count.first.number
+  erb :index
+end
+
+post '/plus' do
+  Count.first.number = Count.first.number + 1
+end  
+
+post '/minus' do
+  Count.first.number = Count.first.number - 1
+end
+
+post '/clear' do
+  Count.first.number = 0
+end
+
+post '/times' do
+  Count.first.number = Count.first.number * 2
+end
+
+post '/divided' do
+  Count.first.number = Count.first.number / 2
+end
+
